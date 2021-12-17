533d210fd52a38f7f3cab4d20c239e86123f1b94
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2021-12-17 13:01:49 +0330

    Move into Center.

--- Label-Constraints-(Beginner)/ViewController.swift
+++ Label-Constraints-(Beginner)/ViewController.swift
@@ -12,6 +12,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Create Label
+        
+        let myLabel = UILabel()
+        myLabel.text = "Omid"
+        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(myLabel)
+        
+        // Create Constraints
+        
+        let centerMyLabelX = NSLayoutConstraint(item: myLabel,
+                                         attribute: .centerX,
+                                               relatedBy: .equal,
+                                         toItem: view,
+                                               attribute: .centerX,
+                                         multiplier: 1,
+                                         constant: 0)
+        
+        let centerMyLabelY = NSLayoutConstraint(item: myLabel,
+                                         attribute: .centerY,
+                                               relatedBy: .equal,
+                                         toItem: view,
+                                               attribute: .centerY,
+                                         multiplier: 1,
+                                         constant: 0)
+        
+        view.addConstraints([centerMyLabelX, centerMyLabelY])
+         
     }
 
 
